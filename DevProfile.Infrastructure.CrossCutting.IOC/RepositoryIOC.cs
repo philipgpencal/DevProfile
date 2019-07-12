9537637af68720f9c1b9e2c4a91e525672279a28
Philip Pencal <ext.philip.pencal@farfetch.com>
2019-07-12 14:17:32 +0100

    change repository injection.

--- DevProfile.Infrastructure.CrossCutting.IOC/RepositoryIOC.cs
+++ DevProfile.Infrastructure.CrossCutting.IOC/RepositoryIOC.cs
@@ -8,17 +8,17 @@ namespace DevProfile.Infrastructure.CrossCutting.IOC
     {
         public static void Load(ContainerBuilder builder)
         {
-            /*
             builder.RegisterType<TechnologyRepository>().As<ITechnologyRepository>();
             builder.RegisterType<SkillRepository>().As<ISkillRepository>();
             builder.RegisterType<StackRepository>().As<IStackRepository>();
             builder.RegisterType<DeveloperRepository>().As<IDeveloperRepository>();
-            */
 
+            /*
             builder.RegisterType<TechnologyMemoryRepository>().As<ITechnologyRepository>();
             builder.RegisterType<SkillMemoryRepository>().As<ISkillRepository>();
             builder.RegisterType<StackMemoryRepository>().As<IStackRepository>();
             builder.RegisterType<DeveloperMemoryRepository>().As<IDeveloperRepository>();
+            */
         }
     }
 }
